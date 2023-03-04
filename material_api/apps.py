2383ad3344a6c60a09b93594a621f1ee608607e5
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-03-04 21:14:56 +0100

    added api app for the model

---
+++ material_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MaterialApiConfig(AppConfig):
+    name = 'material_api'
---
+++ material_api/views.py
@@ -0,0 +1,50 @@
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from base.models import Course, Topic, Material
+from .serializers import CourseSerializer, TopicSerializer, MaterialSerializer
+from .permissions import IsAdminOrReadOnly
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAdminUser
+
+
+class CourseList(generics.ListCreateAPIView):
+    authentication_classes = [SessionAuthentication, JWTAuthentication, IsAdminOrReadOnly]
+    permission_classes = [IsAdminUser]
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [SessionAuthentication, JWTAuthentication, IsAdminOrReadOnly]
+    permission_classes = [IsAdminUser]
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class TopicList(generics.ListCreateAPIView):
+    authentication_classes = [SessionAuthentication, JWTAuthentication, IsAdminOrReadOnly]
+    permission_classes = [IsAdminUser]
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+
+
+class TopicDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [SessionAuthentication, JWTAuthentication, IsAdminOrReadOnly]
+    permission_classes = [IsAdminUser]
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+
+
+class MaterialList(generics.ListCreateAPIView):
+    authentication_classes = [SessionAuthentication, JWTAuthentication, IsAdminOrReadOnly]
+    permission_classes = [IsAdminUser]
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
+
+
+class MaterialDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [SessionAuthentication, JWTAuthentication, IsAdminOrReadOnly]
+    permission_classes = [IsAdminUser]
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer

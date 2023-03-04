7f18d2131fa92b208e7ccb8996b5d3d2118969b8
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-03-04 23:16:07 +0100

    added an allowed host in settings

--- Bstore/settings.py
+++ Bstore/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # This setting tells Django at which URL static files are going to be served to the user.
 # Here, they well be accessible at your-domain.onrender.com/static/...
+STATIC_URL = '/static/'
 # Following settings only make sense on production and may break development environments.
     # in your application directory on Render.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

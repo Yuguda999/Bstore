08e2cf6fe69ae5bf9b71bd66ac57f8e75736ce47
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-03-05 00:07:33 +0100

    added an allowed host in settings

--- Bstore/settings.py
+++ Bstore/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Following settings only make sense on production and may break development environments.
     # in your application directory on Render.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
